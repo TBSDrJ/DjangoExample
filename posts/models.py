8134c97cc4b16555d67cf46973449bcfe6ac9c2c
TBSDrJ <marcus.jaiclin@bishops.com>
2020-03-29 12:36:57 -0700

    Comment out old Post model, copy __str()__ method to PostTemp model.

--- posts/models.py
+++ posts/models.py
@@ -6,13 +6,39 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+# class Post(models.Model):
+    # """
+    # This model will contain the data for a single post to Twit.
+    # """
+    # postText = models.CharField(max_length=280)
+    # # This matches up with the 'username' field of the built-in User model.
+    # userPosted = models.CharField(max_length=50)
+    # pubDate = models.DateTimeField('date published')
+    # likes = models.IntegerField(default=0)
+    #
+    # def __str__(self):
+    #     # I'm using a more complete stringification of the Post that shows
+    #     # the first 40 characters of the text, the id number of the post, and
+    #     # the time/date it was published.
+    #     postStr = "\n\t" + "Id: " + str(self.id)
+    #     if len(self.postText) < 40:
+    #         postStr += "\n\t" + "postText: " + self.postText
+    #     else:
+    #         postStr += "\n\t" + "postText: " + self.postText[:40] + "..."
+    #     postStr += "\n\t" + "Date Published: " + str(self.pubDate)
+    #     postStr += "\n"
+    #     return postStr
+
+class PostTemp(models.Model):
     """
-    This model will contain the data for a single post to Twit.
+    Creating this model to replace the old Post model.
+
+    Trying to switch userPosted from a character field that stores the
+    username to a ForiegnKey field that links to the User object.
     """
     postText = models.CharField(max_length=280)
     # This matches up with the 'username' field of the built-in User model.
-    userPosted = models.CharField(max_length=50)
+    userPosted = models.ForeignKey(User, on_delete=models.CASCADE)
     pubDate = models.DateTimeField('date published')
     likes = models.IntegerField(default=0)
 
@@ -28,16 +54,3 @@ class Post(models.Model):
         postStr += "\n\t" + "Date Published: " + str(self.pubDate)
         postStr += "\n"
         return postStr
-
-class PostTemp(models.Model):
-    """
-    Creating this model to replace the old Post model.
-
-    Trying to switch userPosted from a character field that stores the
-    username to a ForiegnKey field that links to the User object.
-    """
-    postText = models.CharField(max_length=280)
-    # This matches up with the 'username' field of the built-in User model.
-    userPosted = models.ForeignKey(User, on_delete=models.CASCADE)
-    pubDate = models.DateTimeField('date published')
-    likes = models.IntegerField(default=0)
